8d348d85be8dc3c1c4771e13bc4daf5d0fcebf87
mohammad <mohammad@localhost.localdomain>
2017-03-05 14:21:05 +0330

    fixing the repo

--- Accounting/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from Accounting import views
-
-urlpatterns = [
-    url(r'^ShowCreditCard/', view=views.ShowCreditCard, name='ShowCreditCard'),
-    url(r'^add_income_source/', view=views.add_income_source, name='HomePage'),
-    url(r'^ShowCostsGraphical/', view=views.show_costs_graphical, name='ShowCosts'),
-]
